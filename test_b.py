df555c2b1240e8c55272297a9bc11d3cf65ae1d6
nniewiadowska <116592667+nniewiadowska@users.noreply.github.com>
2022-11-08 21:13:16 +0100

    Update test_b.py

--- test_b.py
+++ test_b.py
@@ -2,6 +2,6 @@ import unittest
 
 from b import adding
 
-class test_program(unittest.TestCase):
+class Test_program(unittest.TestCase):
   def test_adding(self):
     self.assertEqual(adding(1, 2, 6, 8))
