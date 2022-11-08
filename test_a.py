48883fa4f141b948f2d6badfbc6cee7dab0bf8de
nniewiadowska <116592667+nniewiadowska@users.noreply.github.com>
2022-11-08 21:12:36 +0100

    Update test_a.py

--- test_a.py
+++ test_a.py
@@ -2,7 +2,7 @@ import unittest
 
 from a import multiply
 
-class test_program(unittest.TestCase):
+class Test_program(unittest.TestCase):
   def test_multiply(self):
     self.assertEqual(multiply(3, 5, 7))
     
